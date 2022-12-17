9949fb81fd72561bf08fb628c53dd7fd872900e4
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2022-12-17 09:35:01 +0700

    fix build

--- src/domain/domain.manager.interface.ts
+++ src/domain/domain.manager.interface.ts
@@ -1,7 +1,5 @@
 import FixturesManager from "../interfaces/fixtures";
-import HelloManager from "../interfaces/hello";
 
 export default interface DomainManagerInterface {
-  helloManager(): HelloManager
   fixturesManager(): FixturesManager
 }
--- src/domain/domain.manager.ts
+++ src/domain/domain.manager.ts
@@ -1,17 +1,12 @@
 import { ConfigProviderInterface } from "../config/config.provider.interface";
 import FixturesManager from "../interfaces/fixtures";
-import hello from "../interfaces/hello";
 import DomainManagerInterface from "./domain.manager.interface";
 import FixturesDomain from "./fixtures/fixtures.domain";
-import HelloDomain from "./hello/hello.domain";
 
 export default class DomainManager implements DomainManagerInterface {
   configProvider: ConfigProviderInterface
   constructor(configProvider: ConfigProviderInterface) {
     this.configProvider = configProvider
-  }
-  helloManager(): hello {
-    return new HelloDomain(this.configProvider)
   }
   fixturesManager(): FixturesManager {
     return new FixturesDomain(this.configProvider)
